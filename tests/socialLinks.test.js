86b20a7165ad32e89d9efe08e2265cdad8fc0474
CrzyHaXs91 <137912394+CrzyHAX91@users.noreply.github.com>
2025-06-05 17:55:58 +0200

    test: check playlist on music page

--- tests/socialLinks.test.js
+++ tests/socialLinks.test.js
@@ -8,7 +8,7 @@ test('contact page has updated social links', () => {
   expect(html).toContain('https://soundcloud.com/thebadguyhimself');
 });
 
-test('index page embeds the SoundCloud playlist', () => {
-  const html = fs.readFileSync(path.join(__dirname, '..', 'index.html'), 'utf8');
+test('music page embeds the SoundCloud playlist', () => {
+  const html = fs.readFileSync(path.join(__dirname, '..', 'music.html'), 'utf8');
   expect(html).toContain('https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/2019398421');
 });
